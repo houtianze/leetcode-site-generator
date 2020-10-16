ae993d8e39e4518150920bdeaf0633c7493fb33f
Hou Tianze <houtianze@users.noreply.github.com>
2020-10-16 22:51:52 +0800

    Change to Hugo

---
+++ packages/hugo-site/scripts/generateDocs.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+const path = require('path');
+const problems = require('../../../problems.json');
+const discussions = require('../../../discussions.json');
+
+const convertTags = tags => tags.map(tag => `- ${tag.name} (${tag.slug})`).join('\n');
+
+const convertCompanyTagStats = (tagStats) => {
+  const tags = [];
+  Object.keys(tagStats).forEach((tagIndex) => {
+    tagStats[tagIndex].forEach((tag) => {
+      tags.push(`- ${tag.name} - ${tag.timesEncountered} (taggedByAdmin: ${tag.taggedByAdmin})`);
+    });
+  });
+
+  return tags.join('\n');
+};
+
+const epochToLocalDate = (epoch) => {
+  const date = new Date(0);
+  date.setUTCSeconds(epoch);
+  return date;
+};
+
+const getDiscussions = (slug) => {
+  const { topics } = discussions[slug];
+  const posts = topics.map((topic) => {
+    const str = `### ${topic.title}
+- Author: ${topic.post.author.username}
+- Creation Date: ${epochToLocalDate(topic.post.creationDate)}
+- Update Date: ${epochToLocalDate(topic.post.updationDate)}
+
+<p>
+${topic.post.content
+    .replace(/\\n/g, '\n')
+    .replace(/\\t/g, '\t')
+    .replace(/\\\\/g, '\\')}
+</p>
+`;
+    return str;
+  });
+
+  return posts.join('\n\n');
+};
+
+const toDoc = ({
+  title,
+  titleSlug,
+  lang = '',
+  code,
+  content,
+  solution,
+  topicTags,
+  companyTagStats,
+}, index) => {
+  const str = (
+    // eslint-disable-next-line
+`---
+title: "${title}"
+weight: ${index}
+#id: "${titleSlug}"
+---
+## Description
+<div class="description">
+${content}
+</div>
+
+## Tags
+${convertTags(topicTags)}
+
+## Companies
+${convertCompanyTagStats(JSON.parse(companyTagStats))}
+
+## Official Solution
+${solution ? solution.content : 'N.A.'}
+
+## Accepted Submission (${lang || 'N.A.'})
+\`\`\`${lang || ''}
+${code || 'N.A.'}
+\`\`\`
+
+## Top Discussions
+${getDiscussions(titleSlug)}
+
+`
+  );
+  return str;
+};
+
+// so not to irate the Liquid engine in Jekyll
+// const patchLiquid = text => text.replace(/\{\{/g, '{ {').replace(/\{%/g, '{ %');
+const patchLiquid = text => text;
+
+const docDirPath = path.join(__dirname, '..', 'content', 'docs');
+
+if (!fs.existsSync(docDirPath)) {
+  fs.mkdirSync(docDirPath);
+}
+
+problems.forEach((problem, problemIndex) => {
+  const indexOneBased = problemIndex + 1;
+  const filename = `${indexOneBased.toString().padStart(6, '0')}-${problem.titleSlug}.md`;
+  fs.writeFile(
+    path.join(docDirPath, filename),
+    patchLiquid(toDoc(problem, indexOneBased)),
+    (err) => {
+      if (err) {
+        console.error(`Write ${filename} error`);
+      }
+    },
+  );
+});
+
+module.exports = {
+  docDirPath,
+};
